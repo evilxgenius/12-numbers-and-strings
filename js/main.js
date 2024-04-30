b9b543792462a394b3e2820f3a35d2cb503dd182
Sergey E <evilx.emelyanov@gmail.com>
2024-04-30 23:40:12 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const userStrings = (length = 3) => {
+    // ... - спред оператор допоможе створити "пустий" (заповнений undefined) масив, що ітерується
+    return [...Array(length)].map((_i, index) => prompt(`#${index + 1} string:`));
+};
+
+const userNumbers = (length = 5) => {
+    let input;
+
+    do {
+        input = prompt('5 digits number');
+
+        if (input === null) return;
+    } while (isNaN(input) || input.length !== length)
+
+    return input;
+};
+
+document.querySelector('.strings').addEventListener('click', () => {
+    const output = userStrings().sort(() => Math.random() - 0.5).join('');
+
+    alert(output);
+});
+
+document.querySelector('.numbers').addEventListener('click', () => {
+    const numbers = userNumbers();
+    const output = numbers ? numbers.split('').join(' ') : 'Ok, bye!'
+
+    alert(output);
+});
